dccc24f5571e4c15fc32fcaf5bde04508d9c8b2f
Henne <henne90gen@gmail.com>
2018-11-25 15:08:05 +0100

    Adding .pylintrc

--- sfm/__main__.py
+++ sfm/__main__.py
@@ -42,6 +42,10 @@ def get_frames_from_video():
 
 
 def calculate_feature_points(frames: list):
+    """
+    Goes through all frames from the given list and calculates features points on each frame.
+    The resulting feature points are then added to the frame together with their descriptors.
+    """
     detector = cv2.BRISK_create()
     for frame in frames:
         blurred = cv2.GaussianBlur(frame.image, (3, 3), 0)
@@ -60,23 +64,22 @@ def main():
     frames = get_frames_from_video()
     frames = calculate_feature_points(frames)
 
-    FLANN_INDEX_KDTREE = 1
-    index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
-    search_params = dict(checks=50)
+    # FLANN_INDEX_KDTREE = 1
+    # index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+    # search_params = dict(checks=50)
 
-    matcher = cv2.FlannBasedMatcher(index_params, search_params)
+    # matcher = cv2.FlannBasedMatcher(index_params, search_params)
+    matcher = cv2.BFMatcher_create()
 
     for i in range(len(frames) - 1):
         frame = frames[i]
         next_frame = frames[i + 1]
         descriptors = frame.features[1]
         next_descriptors = next_frame.features[1]
-        if not descriptors.any():
+        if not descriptors.any() or not next_descriptors.any():
             continue
-        if not next_descriptors.any():
-            continue
-        print("Matching")
-        matches = matcher.knnMatch(descriptors, next_descriptors, k=2)
+
+        matches = matcher.knnMatch(descriptors, next_descriptors, k=1)
 
         draw_params = dict(matchColor=(0, 255, 0),
                            singlePointColor=(255, 0, 0),
